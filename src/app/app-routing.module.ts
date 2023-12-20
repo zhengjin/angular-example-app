1094c5c972ea8608d65224b96fc236ac59e322f6
zhengjin <zhengjin@300.cn>
2023-12-20 11:18:19 +0800

    import openlayer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'map', component: MapComponent },
   { path: '**', redirectTo: "home" }
 ];
 
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.initMap()
   }
 
+  initMap() {
+    const map = new Map({
+      target: 'map',
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+
+    const osmLayer = new TileLayer({
+      source: new OSM()
+    });
+
+    map.addLayer(osmLayer);
+  }
+
+  
 }
