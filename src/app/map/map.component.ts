97b11d2d6d725001593dc4bbb516491cd6c8f060
zhengjin <zhengjin@300.cn>
2023-12-29 10:36:38 +0800

    update map init method

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import Map from 'ol/Map';
-import View from 'ol/View';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import HeatmapLayer from 'ol/layer/Heatmap';
+import VectorSource from 'ol/source/Vector';
+// import GeoJSON from 'ol/format/GeoJSON';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import Overlay from 'ol/Overlay';
 import OSM from 'ol/source/OSM';
 
 @Component({
@@ -9,21 +15,60 @@ import OSM from 'ol/source/OSM';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
+  @ViewChild('map', { static: true }) mapElement!: ElementRef;
+  @ViewChild('popupContent') popupContent: ElementRef;
 
-  constructor() {}
+  private map!: Map;
+  private vectorSource: VectorSource;
+  private vectorLayer: VectorLayer<VectorSource>;
+  private heatmapLayer: HeatmapLayer;
+  private overlay: Overlay;
+
+  constructor() {
+    this.popupContent = new ElementRef<HTMLDivElement>(document.createElement('div'));
+
+    this.vectorSource = new VectorSource();
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+    });
+    this.heatmapLayer = new HeatmapLayer({
+      source: this.vectorSource,
+      radius: 15,
+      blur: 25,
+    });
+    this.overlay = new Overlay({
+      element: this.popupContent.nativeElement,
+      autoPan: true,
+      positioning: 'bottom-center',
+      stopEvent: false
+    });
+  }
 
   ngOnInit(): void {
     this.initMap()
   }
 
+  ngAfterViewInit(): void {
+    const mapElement = document.getElementById('map');
+    if (mapElement) {
+      this.mapElement = new ElementRef(mapElement as HTMLDivElement);
+      this.initMap();
+    } else {
+      console.error('Unable to find the map element with id "map" in the DOM.');
+    }
+  }
+
   initMap() {
+    const baiduSource = new TileLayer();
     const map = new Map({
-      target: 'map',
+      target: this.mapElement.nativeElement,
+      layers: [baiduSource, this.vectorLayer, this.heatmapLayer],
       view: new View({
         center: [0, 0],
-        zoom: 2
-      })
+        zoom: 2,
+        minZoom: 3,
+      }),
     });
 
     const osmLayer = new TileLayer({
